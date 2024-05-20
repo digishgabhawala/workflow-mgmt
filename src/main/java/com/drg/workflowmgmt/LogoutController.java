f17e4c530d27d666a42097f8b98220051c95a78e
digish <digish_gabhawala@yahoo.com>
2024-05-20 18:52:15 +0530

    logout fix

--- src/main/java/com/drg/workflowmgmt/LogoutController.java
+++ src/main/java/com/drg/workflowmgmt/LogoutController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LogoutController {
 
-    @GetMapping("/logout")
+    @GetMapping("/l34ogout")
     public String logout() {
         return "redirect:/perform_logout";
     }

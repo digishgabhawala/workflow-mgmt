36a387318de7df398738b0b3ffbeeb0fb5c2402a
digish <digish_gabhawala@yahoo.com>
2024-06-17 22:01:58 +0530

    added card in jobs

--- src/main/resources/static/jobs.js
+++ src/main/resources/static/jobs.js
@@ -132,19 +132,18 @@ async function addJobTransition(jobId, fromStateId, toStateId, csrfToken) {
 }
 
 window.onload = async function() {
-    loadJobs();
+    await loadJobs();
     await loadJobStates();
-    // Add event listeners to the Show/Hide buttons
-    document.getElementById('toggleJobStatesTableBtn').addEventListener('click', toggleJobStatesTable);
-    document.getElementById('toggleJobsTableBtn').addEventListener('click', toggleJobsTable);
-    // Initially hide the Job States table
-    jobStatesTable.style.display = 'none';
-    jobsTable.style.display = 'table';
-    // Set initial icons
-    setIcon('jobStatesTableIcon', 'fas fa-eye'); // Show icon
-    setIcon('jobsTableIcon', 'fas fa-eye-slash'); // Show icon
-//    toggleJobStatesTable();
+
+    // Set up event listener for each job name header
+    const jobNameHeaders = document.querySelectorAll('.job-name-header');
+    jobNameHeaders.forEach(header => {
+        header.addEventListener('click', () => toggleJobCard(header));
+    });
 };
+
+
+
 
 
 // Function to toggle visibility of the Job States table
@@ -270,3 +269,361 @@ async function handleRemoveJobState(event, jobId, jobStateId) {
         showAlertModal('Error', 'Failed to remove job state');
     }
 }
+
+async function fetchJobState(jobStateId) {
+    const response = await fetch(`/jobstates/${jobStateId}`);
+    return response.json();
+}
+
+async function fetchJobStates() {
+    const response = await fetch('/jobstates');
+    return response.json();
+}
+
+async function createJobState(jobStateName, csrfToken) {
+    const response = await fetch('/jobstates', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': csrfToken
+        },
+        body: JSON.stringify({ name: jobStateName })
+    });
+    return response.json();
+}
+
+
+async function updateJobState(jobStateId, jobState, csrfToken) {
+    const response = await fetch(`/jobstates/${jobStateId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': csrfToken
+        },
+        body: JSON.stringify(jobState)
+    });
+    return response.json();
+}
+
+async function addRoleToJobState(jobStateId, roleName, csrfToken) {
+    const jobState = await fetchJobState(jobStateId);
+    if (!jobState.roles.includes(roleName)) {
+        jobState.roles.push(roleName);
+        await updateJobState(jobStateId, jobState, csrfToken);
+    }
+}
+
+async function removeRoleFromJobState(jobStateId, roleName, csrfToken) {
+    const jobState = await fetchJobState(jobStateId);
+    const roleIndex = jobState.roles.indexOf(roleName);
+    if (roleIndex > -1) {
+        jobState.roles.splice(roleIndex, 1);
+        await updateJobState(jobStateId, jobState, csrfToken);
+    }
+}
+
+// Ensure the role dropdown is populated with roles not already assigned to the job state
+async function populateRoleDropdown(jobStateId) {
+    const roles = await fetchRoles();
+    const jobState = await fetchJobState(jobStateId);
+    const dropdown = document.getElementById(`roleDropdown-${jobStateId}`);
+    dropdown.innerHTML = '';
+
+    // Filter roles that are not already assigned to the job state
+    const availableRoles = roles.filter(role => !jobState.roles.includes(role.name));
+
+    availableRoles.forEach(role => {
+        const option = document.createElement('option');
+        option.value = role.id;
+        option.textContent = role.name;
+        dropdown.appendChild(option);
+    });
+}
+
+
+async function loadJobs() {
+    const jobs = await fetchJobs();
+    const jobStates = await fetchJobStates();
+    const jobsCards = document.getElementById('jobsCards');
+    jobsCards.innerHTML = '';
+
+    jobs.forEach(job => {
+        const { availableJobStates, existingJobStates } = filterJobStates(jobStates, job);
+        const jobStateOptions = generateJobStateOptions(availableJobStates);
+        const jobTransitionOptions = generateJobStateOptions(existingJobStates);
+
+        const jobStatesList = generateJobStatesList(job);
+        const transitionsList = generateTransitionsList(job, jobStates);
+
+        const card = createJobCard(job, jobStateOptions, jobTransitionOptions, jobStatesList, transitionsList);
+        jobsCards.appendChild(card);
+    });
+
+    loadJobStates(); // Ensure job states are loaded after jobs
+    populateJobStateDropdowns(); // Populate dropdowns for start and end states
+}
+
+
+function populateJobStateDropdowns() {
+    const jobStateSelectElements = ['startState', 'endState'];
+    jobStateSelectElements.forEach(async (elementId) => {
+        const selectElement = document.getElementById(elementId);
+        const jobStates = await fetchJobStates();
+        selectElement.innerHTML = generateJobStateOptions(jobStates);
+    });
+}
+
+function filterJobStates(jobStates, job) {
+    const availableJobStates = [];
+    const existingJobStates = [];
+    for (let state of jobStates) {
+        let isPartOfJob = false;
+        for (let jobState of job.jobStates) {
+            if (jobState.id === state.id) {
+                isPartOfJob = true;
+                break;
+            }
+        }
+        if (!isPartOfJob) {
+            availableJobStates.push(state);
+        } else {
+            existingJobStates.push(state);
+        }
+    }
+    return { availableJobStates, existingJobStates };
+}
+
+function generateJobStateOptions(states) {
+    return states.map(state => `<option value="${state.id}">${state.name}</option>`).join('');
+}
+
+function generateJobStatesList(job) {
+    const startStateId = job.startState.id;
+    const endStateId = job.endState.id;
+
+    return job.jobStates.map(state => {
+        let removeButton = ''; // Initialize removeButton variable
+
+        // Check if the state is the start or end state
+        if (state.id === startStateId || state.id === endStateId) {
+            // Disable the remove button if it's the start or end state
+            removeButton = `<button class="btn btn-sm btn-danger ml-2" disabled>Remove</button>`;
+        } else {
+            // Render the remove button normally if it's not the start or end state
+            removeButton = `<button class="btn btn-sm btn-danger ml-2" onclick="handleRemoveJobState(event, ${job.id}, ${state.id})">Remove</button>`;
+        }
+
+        return `
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                ${state.name}
+                ${removeButton}
+            </li>
+        `;
+    }).join('');
+}
+
+function generateTransitionsList(job, jobStates) {
+    return job.fromJobStateIds.map((fromStateId, index) => {
+        const fromState = jobStates.find(state => state.id === fromStateId);
+        const toState = jobStates.find(state => state.id === job.toJobStateIds[index]);
+        return fromState && toState ? `
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                ${fromState.name} -> ${toState.name}
+                <button class="btn btn-sm btn-danger ml-2" onclick="handleRemoveTransition(event, ${job.id}, ${fromStateId}, ${job.toJobStateIds[index]})">Remove</button>
+            </li>
+        ` : '';
+    }).join('');
+}
+
+function createJobRow(job, jobStateOptions, jobTransitionOptions, jobStatesList, transitionsList) {
+    const addJobStateForm = `
+        <button class="btn btn-sm btn-primary btn-block" onclick="document.getElementById('addJobStateForm-${job.id}').classList.toggle('d-none')">
+            <i class="fas fa-plus"></i> Add Job State
+        </button>
+        <form id="addJobStateForm-${job.id}" class="form-inline mt-2 d-none" onsubmit="handleAddJobState(event, ${job.id})">
+            <select id="jobStateSelect-${job.id}" class="form-control mr-2 jobStateDropdown">${jobStateOptions}</select>
+            <button type="submit" class="btn btn-primary btn-sm">Add</button>
+        </form>
+    `;
+
+    const addTransitionForm = `
+        <button class="btn btn-sm btn-primary btn-block" onclick="document.getElementById('transitionForm-${job.id}').classList.toggle('d-none')">
+            <i class="fas fa-plus"></i> Add Transition
+        </button>
+        <form id="transitionForm-${job.id}" class="d-none mt-2" onsubmit="handleAddTransition(event, ${job.id})">
+            <div class="form-group">
+                <label for="fromState-${job.id}">From State:</label>
+                <select id="fromState-${job.id}" class="form-control">${jobTransitionOptions}</select>
+            </div>
+            <div class="form-group">
+                <label for="toState-${job.id}">To State:</label>
+                <select id="toState-${job.id}" class="form-control">${jobTransitionOptions}</select>
+            </div>
+            <button type="submit" class="btn btn-primary">Add Transition</button>
+        </form>
+    `;
+
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>${job.id}</td>
+        <td>${job.name}</td>
+        <td>
+            <ul class="list-group">
+                <li class="list-group-item">
+                    ${addJobStateForm}
+                </li>
+                ${jobStatesList}
+            </ul>
+        </td>
+        <td>
+            <ul class="list-group">
+                <li class="list-group-item">
+                    ${addTransitionForm}
+                </li>
+                ${transitionsList}
+            </ul>
+        </td>
+    `;
+    return row;
+}
+
+async function handleSubmitJob(event) {
+    event.preventDefault();
+
+    const jobName = document.getElementById('jobName').value;
+    const startStateId = document.getElementById('startState').value;
+    const endStateId = document.getElementById('endState').value;
+    const csrfToken = await fetchCsrfToken();
+
+    const job = {
+        name: jobName,
+        startState: { id: startStateId }, // Pass startState and endState as objects
+        endState: { id: endStateId }
+    };
+
+    try {
+        response = await createJob(job, csrfToken);
+        if(response.id){
+            loadJobs();
+            document.getElementById('jobForm').reset();
+            document.getElementById('jobForm').classList.add('d-none');
+            document.getElementById('showJobFormButton').style.display = 'inline-block';
+        } else if(response.message){
+            showAlertModal('Error',response.message);
+        }
+    } catch (error) {
+        console.error('Error creating job:', error);
+        showAlertModal('Error','Failed to create job. Please try again.');
+    }
+}
+
+async function createJob(job, csrfToken) {
+    const response = await fetch('/jobs', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': csrfToken
+        },
+        body: JSON.stringify(job)
+    });
+
+    return response.json();
+}
+async function fetchJobs() {
+    const response = await fetch('/jobs');
+    const data = await response.json();
+    return data;
+}
+
+// Function to toggle visibility of the Jobs cards
+function toggleJobsCards() {
+    const jobsCards = document.getElementById('jobsCards');
+    if (jobsCards.style.display === 'none' || !jobsCards.style.display) {
+        jobsCards.style.display = 'block';
+        setIcon('jobsCardsIcon', 'fas fa-eye-slash'); // Hide icon
+    } else {
+        jobsCards.style.display = 'none';
+        setIcon('jobsCardsIcon', 'fas fa-eye'); // Show icon
+    }
+}
+
+function createJobCard(job, jobStateOptions, jobTransitionOptions, jobStatesList, transitionsList) {
+    const addJobStateForm = `
+        <button class="btn btn-sm btn-primary btn-block" onclick="document.getElementById('addJobStateForm-${job.id}').classList.toggle('d-none')">
+            <i class="fas fa-plus"></i> Add Job State
+        </button>
+        <form id="addJobStateForm-${job.id}" class="form-inline mt-2 d-none" onsubmit="handleAddJobState(event, ${job.id})">
+            <select id="jobStateSelect-${job.id}" class="form-control mr-2 jobStateDropdown">${jobStateOptions}</select>
+            <button type="submit" class="btn btn-primary btn-sm">Add</button>
+        </form>
+    `;
+
+    const addTransitionForm = `
+        <button class="btn btn-sm btn-primary btn-block" onclick="document.getElementById('transitionForm-${job.id}').classList.toggle('d-none')">
+            <i class="fas fa-plus"></i> Add Transition
+        </button>
+        <form id="transitionForm-${job.id}" class="d-none mt-2" onsubmit="handleAddTransition(event, ${job.id})">
+            <div class="form-group">
+                <label for="fromState-${job.id}">From State:</label>
+                <select id="fromState-${job.id}" class="form-control">${jobTransitionOptions}</select>
+            </div>
+            <div class="form-group">
+                <label for="toState-${job.id}">To State:</label>
+                <select id="toState-${job.id}" class="form-control">${jobTransitionOptions}</select>
+            </div>
+            <button type="submit" class="btn btn-primary">Add Transition</button>
+        </form>
+    `;
+
+    const jobStatesContent = `
+        <div id="jobStates-${job.id}">
+            <h6>Job States</h6>
+            <ul class="list-group mb-3">
+                <li class="list-group-item">
+                    ${addJobStateForm}
+                </li>
+                ${jobStatesList}
+            </ul>
+        </div>
+    `;
+
+    const transitionsContent = `
+        <div id="transitions-${job.id}">
+            <h6>Transitions</h6>
+            <ul class="list-group">
+                <li class="list-group-item">
+                    ${addTransitionForm}
+                </li>
+                ${transitionsList}
+            </ul>
+        </div>
+    `;
+
+    const card = document.createElement('div');
+    card.classList.add('col-md-4', 'mb-4');
+    card.innerHTML = `
+        <div class="card h-100">
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <h5 class="card-title m-0">${job.name}</h5>
+                <i class="fas fa-chevron-down"></i>
+            </div>
+            <div class="card-body d-none">
+                ${jobStatesContent}
+                ${transitionsContent}
+            </div>
+        </div>
+    `;
+
+    const cardHeader = card.querySelector('.card-header');
+    const cardBody = card.querySelector('.card-body');
+
+    cardHeader.addEventListener('click', () => {
+        cardBody.classList.toggle('d-none');
+        const icon = cardHeader.querySelector('.fa-chevron-down');
+        icon.classList.toggle('fa-chevron-up');
+    });
+
+    return card;
+}
+
+
